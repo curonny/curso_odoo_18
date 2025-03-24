da4bcbe9ba6141cf1f23d94e07dc3923bea6d327
Ronny <rmontano1992@gmail.com>
2025-03-24 22:32:48 +0100

    [VID3] Add sale

---
+++ estate_sale/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Sale Real Estate",
+    "summary": "Real Estate",
+    "author": "Ronny Montano",
+    "license": "AGPL-3",
+    "website": "https://github.com/curonny/curso_odoo_18",
+    "category": "Real Estate",
+    "version": "18.0.0.0.1",
+    "depends": ["estate", "sale_management"],
+    "data": [
+        "views/view_order_form.xml",
+    ],
+    "demo": [],
+}
